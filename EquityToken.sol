772fab18e368adbfaefa84156f1760b63eb5728a
jeffsenk <jeff.senk@gmail.com>
2018-05-16 10:06:32 -0500

    added safe mul and div functions

--- EquityToken.sol
+++ EquityToken.sol
@@ -27,10 +27,13 @@ contract EquityToken{
     uint256 windDownVotes;
 
     event Transfer(address indexed from, address indexed to, uint tokens);
+    event AdminElected(address newAdmin, uint votes);
+    event DividendDistributed(uint dividend);
+    event BudgetDistributed(address target,uint budget);
 
-    constructor(uint256 _totalSupply, uint256 _quorum, uint256 _dividendPeriod,
+    constructor(uint256 initSupply, uint256 _quorum, uint256 _dividendPeriod,
     uint256 _budgetPeriod) public{
-        totalSupply_ = _totalSupply;
+        totalSupply_ = initSupply;
         quorum = _quorum;
         administrator = msg.sender;
         tokenBalances[administrator] = totalSupply_;
@@ -39,6 +42,8 @@ contract EquityToken{
         dividendPeriod = _dividendPeriod;
         budgetPeriod = _budgetPeriod;
     }
+
+    /* math functions from OpenZeppelin SafeMath.sol**/
 
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
       assert(b <= a);
@@ -51,9 +56,28 @@ contract EquityToken{
       return c;
     }
 
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+      if (a == 0) {
+        return 0;
+      }
+      uint256 c = a * b;
+      assert(c / a == b);
+      return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 c = a / b;
+      return c;
+    }
+
     modifier onlyAdmin() {
       require(msg.sender == administrator);
       _;
+    }
+
+    function calcShare(uint256 memberBalance,uint256 payOut)internal view returns (uint256){
+      uint256 percentage = div(memberBalance,totalSupply_);
+      return mul(payOut,percentage);
     }
 
     function totalSupply() public view returns (uint256) {
@@ -63,11 +87,8 @@ contract EquityToken{
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
         require(_value <= tokenBalances[msg.sender]);
-
         tokenBalances[msg.sender] = sub(tokenBalances[msg.sender],_value);
         tokenBalances[_to] = add(tokenBalances[_to],_value);
-
-        /**Must check if _to is current member */
         bool isMember = false;
         for(uint i=0;i<members.length;i++){
             if(members[i] == _to){
@@ -79,7 +100,6 @@ contract EquityToken{
             members.push(_to);
         }
         /**May also want to remove msg.sender if tokenBalances[msg.sender]<=0*/
-
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
@@ -104,10 +124,10 @@ contract EquityToken{
         require(now > add(lastDividend,dividendPeriod));
         lastDividend = now;
         for(uint i =0;i<members.length;i++){
-            uint256 share = dividend * (tokenBalances[members[i]]/totalSupply_);
-            /**rounding*/
+            uint256 share = calcShare(tokenBalances[members[i]],dividend);
             ethBalances[members[i]] = add(ethBalances[members[i]],share);
         }
+        emit DividendDistributed(dividend);
         dividend = 0;
         return true;
     }
@@ -128,6 +148,7 @@ contract EquityToken{
         require(now > add(lastBudget,budgetPeriod));
         lastBudget = now;
         ethBalances[_to] = add(ethBalances[_to],budget);
+        emit BudgetDistributed(_to,budget);
         budget = 0;
         return true;
     }
@@ -139,6 +160,7 @@ contract EquityToken{
         candidateVotes[candidate] = add(candidateVotes[candidate],tokenBalances[msg.sender]);
         if(candidateVotes[candidate] > quorum){
             administrator = candidate;
+            emit AdminElected(administrator,candidateVotes[candidate]);
         }
         return true;
     }
@@ -148,7 +170,7 @@ contract EquityToken{
     }
 
     function withdraw() public returns (bool){
-      //pay out full balance of calling member
+       /*pay out full balance of calling member**/
        uint amountToWithdraw = ethBalances[msg.sender];
        ethBalances[msg.sender] = 0;
        msg.sender.transfer(amountToWithdraw);
@@ -159,8 +181,7 @@ contract EquityToken{
         windDownVotes = add(windDownVotes,tokenBalances[msg.sender]);
         if(windDownVotes > quorum){
             for(uint i =0;i<members.length;i++){
-              uint256 share = address(this).balance * (tokenBalances[members[i]]/totalSupply_);
-              /**rounding*/
+              uint256 share = calcShare(tokenBalances[members[i]],address(this).balance);
               ethBalances[members[i]] = add(ethBalances[members[i]],share);
             }
         }
